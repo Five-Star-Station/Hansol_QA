05067246056ff16ae00da320ef0c52e6aabecc0d
Harheem Kim <49297157+harheem@users.noreply.github.com>
2024-03-08 12:23:04 +0900

    code merge

---
+++ main.py
@@ -0,0 +1,85 @@
+# Import necessary libraries
+import pandas as pd
+import numpy as np
+import torch
+from kiwipiepy import Kiwi
+from transformers import AutoModelForSequenceClassification, AutoTokenizer
+from langchain.embeddings.huggingface import HuggingFaceEmbeddings
+from langchain.vectorstores import Chroma
+
+# Initialize Kiwi for Korean sentence splitting
+kiwi = Kiwi()
+
+# Load the datasets
+train_df = pd.read_csv("data/train.csv")
+test_df = pd.read_csv("data/test.csv")
+
+
+# Define a function to split queries into sentences using Kiwi
+def split_into_sentences(query):
+    return [item[0] for item in kiwi.split_into_sents(query)]
+
+
+# Apply the function to the test dataset
+test_df["split_queries"] = test_df["질문"].map(split_into_sentences)
+
+# Template for generating prompts
+PROMPT_TEMPLATE = """<s>[INST] <<SYS>>
+You are an helpful assistant for construction and interior. Your task is to generate a valid answer based on the given information:
+
+{context}
+
+<</SYS>>
+{query} [/INST]
+"""
+
+# Initialize embedding and reranking models
+embedding_model_name = "sentence-transformers/distiluse-base-multilingual-cased-v1"
+rerank_model_name = "Dongjin-kr/ko-reranker"
+
+# Set up the vector store for embeddings
+chromadb_store = "vector_store/"
+vectordb = Chroma(
+    persist_directory=chromadb_store,
+    embedding_function=HuggingFaceEmbeddings(model_name=embedding_model_name),
+)
+
+# Load reranking model and tokenizer
+rerank_tokenizer = AutoTokenizer.from_pretrained(rerank_model_name)
+rerank_model = AutoModelForSequenceClassification.from_pretrained(rerank_model_name)
+rerank_model.eval()
+
+
+# Function to normalize scores
+def exp_normalize(scores):
+    b = scores.max()
+    y = np.exp(scores - b)
+    return y / y.sum()
+
+
+# Function to get the best matching document
+def get_first_place_doc(query, matching_docs):
+    pairs = [[query, doc.page_content.split("answer: ")[1]] for doc in matching_docs]
+
+    with torch.no_grad():
+        inputs = rerank_tokenizer(
+            pairs, padding=True, truncation=True, return_tensors="pt", max_length=512
+        )
+        logits = rerank_model(**inputs, return_dict=True).logits.view(-1).float()
+        scores = exp_normalize(logits.numpy())
+        max_idx = scores.argmax()
+
+    return matching_docs[max_idx]
+
+
+# Processing the queries from the test data
+for queries in test_df["split_queries"][:2]:
+    for query in queries:
+        # Get matching documents for the query
+        matching_docs = vectordb.similarity_search(query, k=7)
+        # Find the best document
+        first_place_doc = get_first_place_doc(query, matching_docs)
+        # Generate prompt
+        prompt = PROMPT_TEMPLATE.format(
+            context=first_place_doc.page_content, query=query
+        )
---
+++ vectordb_uploader.py
@@ -0,0 +1,25 @@
+import pandas as pd
+from langchain.embeddings.huggingface import HuggingFaceEmbeddings
+from langchain.vectorstores import Chroma
+from langchain_community.document_loaders.csv_loader import CSVLoader
+import chromadb
+
+
+embedding_model = "sentence-transformers/distiluse-base-multilingual-cased-v1"
+chromadb_store = "vector_store/"
+
+
+loader = CSVLoader(file_path="data/answers.csv", encoding="UTF-8")
+documents = loader.load()
+client = chromadb.Client()
+if client.list_collections():
+    consent_collection = client.create_collection("consent_collection")
+else:
+    print("Collection already exists")
+vectordb = Chroma.from_documents(
+    documents=documents,
+    embedding=HuggingFaceEmbeddings(
+        model_name=embedding_model, encode_kwargs={"normalize_embeddings": True}
+    ),
+    persist_directory=chromadb_store,
+)
